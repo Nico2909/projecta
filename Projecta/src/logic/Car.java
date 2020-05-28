138ff0f3d2208ee825c21ab60a8078b585243e7a
nis <Nico.Stief@dbh.de>
2020-05-28 12:38:57 +0200

    getter und setter hinzugefügt

--- Projecta/src/logic/Car.java
+++ Projecta/src/logic/Car.java
@@ -2,7 +2,24 @@ package logic;
 
 public class Car {
 
+    public int getMileAge() {
+        return mileAge;
+    }
+
+    public void setMileAge(int mileAge) {
+        this.mileAge = mileAge;
+    }
+
     private int mileAge;
+
+    public int getActualSpeed() {
+        return actualSpeed;
+    }
+
+    public void setActualSpeed(int actualSpeed) {
+        this.actualSpeed = actualSpeed;
+    }
+
     private int actualSpeed;
 
     public Car(int mileAge, int actualSpeed) {
